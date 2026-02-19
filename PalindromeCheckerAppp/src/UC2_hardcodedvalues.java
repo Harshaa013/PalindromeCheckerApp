d0543a4f71af82009e0c885b062c656808b6d015
Harshaa013 <sriharsha.s.p1@gmail.com>
2026-02-19 09:43:20 +0530

    Implement palindrome check in UC2_hardcodedvalues

--- PalindromeCheckerAppp/src/UC2_hardcodedvalues.java
+++ PalindromeCheckerAppp/src/UC2_hardcodedvalues.java
@@ -1,2 +1,12 @@
 public class UC2_hardcodedvalues {
-}
+        public static void main(String[] args) {
+            String input = "madam";
+            String reversed = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed += input.charAt(i);
+            }
+            boolean isPalindrome = input.equals(reversed);
+            System.out.println("Input text: " + input);
+            System.out.println("Is it a Palindrome? : " + isPalindrome);
+        }
+    }
